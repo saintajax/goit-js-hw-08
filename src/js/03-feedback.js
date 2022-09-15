11913ed02229b22ec6c6f316d989b0651ab850fb
saintajax <saintajax@gmail.com>
2022-09-16 00:11:13 +0300

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  submit: document.querySelector('[type="submit"]'),
+};
+
+let formData = {};
+
+onPageReload();
+
+refs.form.addEventListener('input', throttle(onInput, 1000));
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
+
+function onInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onPageReload() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    refs.form.querySelector('input').value = formData.email;
+    refs.form.querySelector('textarea').value = formData.message;
+  }
+}
+
+console.log(formData);
