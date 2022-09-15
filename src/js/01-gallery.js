51c41e05de7a2c07bf15d7ee27fb4645d986c851
saintajax <saintajax@gmail.com>
2022-09-15 20:31:27 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const galleryItemMarkup = makeGalleryItemMarkup(galleryItems).join('');
+
+function makeGalleryItemMarkup(galleryItems = {}) {
+  return galleryItems.map(({ description, original, preview }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" loading="lazy" src="${preview}" alt="${description}" />
+</a>`;
+  });
+}
+
+refs.gallery.innerHTML = galleryItemMarkup;
+
+// refs.gallery.addEventListener("click", onOpenModal);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
